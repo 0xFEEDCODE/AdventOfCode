efdf43d7fcb495986f936988734b1c928d12e83e
synca <samo.seman96@gmail.com>
2023-12-04 08:24:52 +0100

    Simplified sol 4

--- Solutions2023/4/Solution4.cs
+++ Solutions2023/4/Solution4.cs
@@ -6,94 +6,58 @@ public class Solution4 : SolutionFramework
 {
     public Solution4() : base(4) { }
 
-    record Numbers(ICollection<int> Winning, ICollection<int> Yours);
-    
     public override string[] Solve()
     {
-        var c = new Dictionary<int, Numbers>();
+        var points = 0;
+        var nCards = 0;
+        
+        var processedCards = new Dictionary<int, int>();
+        
         foreach (var s in RawInputSplitByNl)
         {
             var s1 = s.Split(':');
-            var n = s1.First().Split(' ').Last().ParseInt();
+            var cardN = s1.First().Split(' ').Last().ParseInt();
             var spl = s1.Last().Split(" | ");
             var w = spl.First().Split(' ').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToArray();
             var y = spl.Last().Split(' ').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToArray();
-            var n1 = new Numbers(w, y);
-            c[n] = new Numbers(w, y);
+
+            var c = w.Count(x => y.Contains(x));
+            points += CountPoints(c);
+
+            var cardNQuantity = ProcessCard(processedCards, cardN, 1);
+
+            for (var i = cardN + 1; i < cardN + 1 + c; i++)
+            {
+                ProcessCard(processedCards, i, cardNQuantity);
+            }
         }
         
-        foreach (var kv in c)
-        {
-            var n = kv.Key;
-
-            var count = kv.Value.Winning.Count(c => kv.Value.Yours.Contains(c));
-
-            if (count >= 1)
-            {
-                var points = 1;
-                for (var i = 1; i < count; i++)
-                {
-                    points *= 2;
-                }
-                NumSlot += points;
-            }
-
-        }
-        AssignAnswer1();
-        
-        c = new Dictionary<int, Numbers>();
-        foreach (var s in RawInputSplitByNl)
-        {
-            var s1 = s.Split(':');
-            var n = s1.First().Split(' ').Last().ParseInt();
-            var spl = s1.Last().Split(" | ");
-            var w = spl.First().Split(' ').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToArray();
-            var y = spl.Last().Split(' ').Where(s => int.TryParse(s, out _)).Select(int.Parse).ToArray();
-            var n1 = new Numbers(w, y);
-            c[n] = new Numbers(w, y);
-        }
-
-        var cards = c;
-
-        var cs = cards.Select(kv => (kv.Key, kv.Value)).ToList();
-
-        while (cs.Count != 0)
-        {
-            var newCards = new Dictionary<int, (int q, Numbers numbers)>();
-
-            foreach (var (cardNr, numbers) in cs)
-            {
-                NumSlot++;
-                    
-                var count = numbers.Winning.Count(c => numbers.Yours.Contains(c));
-
-                if (count >= 1)
-                {
-                    for (var i = 0; i < count; i++)
-                    {
-                        var idx = cardNr + i + 1;
-                        var key = newCards.Keys.SingleOrDefault(k => k == idx);
-                        if (key != default)
-                        {
-                            newCards[key] = newCards[key] with{q = newCards[key].q + 1};
-                        } else
-                        {
-                            newCards[idx] = (1, cards[idx]);
-                        }
-                    }
-                }
-            }
-            cs.Clear();
-            foreach (var kv in newCards)
-            {
-                for (var i = 0; i < kv.Value.q; i++)
-                {
-                    cs.Add((kv.Key, kv.Value.numbers));
-                }
-            }
-        }
-        AssignAnswer2();
+        AssignAnswer1(points);
+        AssignAnswer2(processedCards.Values.Sum());
 
         return Answers;
+    }
+    
+    private static int CountPoints(int c)
+    {
+        var points = 1;
+        for (var i = 0; i < c; i++)
+        {
+            points <<= 1;
+        }
+        return points >> 1;
+    }
+
+    private static int ProcessCard(Dictionary<int, int> processedCards, int idx, int quantity)
+    {
+        if (processedCards.ContainsKey(idx))
+        {
+            processedCards[idx] += quantity;
+        }
+        else
+        {
+            processedCards[idx] = quantity;
+        }
+        return processedCards[idx];
     }
 }
