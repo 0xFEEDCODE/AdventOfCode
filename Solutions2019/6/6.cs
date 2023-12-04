e204c45cba2eaa8d55ff8a7112eb36d6e0959874
synca <samo.seman96@gmail.com>
2023-12-04 10:44:36 +0100

    Improvement

--- Solutions2019/6/6.cs
+++ Solutions2019/6/6.cs
@@ -7,10 +7,12 @@ public class Solution6 : SolutionFramework
     public Solution6() : base(6) { }
 
     record Pair(string E1, string E2);
+    record Node(Node? Parent, string Value);
 
     public override string[] Solve()
     {
         var pairs = new List<Pair>();
+        var nodes = new List<Node>();
         
         int Distance(string o1, string o2)
         {
@@ -20,51 +22,66 @@ public class Solution6 : SolutionFramework
             c = o2;
             var pred2 = GetPredecessors(c, pairs);
 
-
-            Console.WriteLine();
-            return 0;
+            var firstCommonPred = pred1.Intersect(pred2).First();
+            var dist1 = pred1.IndexOf(firstCommonPred);
+            var dist2 = pred2.IndexOf(firstCommonPred);
+            
+            return dist1+dist2;
         }
 
-        int PredecessorsCount(Pair pair)
-        {
-            var c = 0;
-            var e = pair.E1;
-            while (!string.IsNullOrEmpty(e))
-            {
-                var o = pairs.SingleOrDefault(p => p.E2 == e);
-                if (o != null)
-                {
-                    c++;
-                    e = o.E1;
-                } else
-                {
-                    e = null;
-                }
-            }
-            return c;
-        }
-        
+        var memoized = new Dictionary<string, int>();
+
         foreach (var s in RawInputSplitByNl)
         {
             var s1 = s.Split(')');
             var obj1 = s1.First();
             var obj2 = s1.Last();
             var pair = new Pair(obj1, obj2);
+
+            var parent = nodes.SingleOrDefault(x => x.Value == obj1);
+            nodes.Add(parent is null ? new Node(new Node(null, obj1), obj2) : new Node(parent, obj2));
+
             pairs.Add(pair);
         }
 
-        var d = Distance("SAN", "YOU");
-
         foreach (var p in pairs)
         {
-            var c = (p, PredecessorsCount(p) + 1);
-            NumSlot += PredecessorsCount(p) + 1;
+            NumSlot += PredecessorsCount(p);
         }
         
         AssignAnswer1();
         
+        NumSlot = Distance("SAN", "YOU");
+        AssignAnswer2();
         
         return Answers;
+
+        int PredecessorsCount(Pair pair)
+        {
+            var pred = pair.E1;
+            var c = 1;
+            
+            while (!string.IsNullOrEmpty(pred))
+            {
+                if (memoized.TryGetValue(pred, out var count))
+                {
+                    return count + c ;
+                }
+                
+                var o = pairs.SingleOrDefault(p => p.E2 == pred);
+                if (o != null)
+                {
+                    c++;
+                    pred = o.E1;
+                    
+                } else
+                {
+                    pred = null;
+                }
+            }
+            memoized[pair.E2] = c;
+            return c;
+        }
     }
     
     private static List<string> GetPredecessors(string c, List<Pair>? pairs)
