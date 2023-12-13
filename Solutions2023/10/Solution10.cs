57ab034f63448a4e01d0b62a6e1d448304eaae94
synca <samo.seman96@gmail.com>
2023-12-13 18:41:34 +0100

    c

--- Solutions2023/10/Solution10.cs
+++ Solutions2023/10/Solution10.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using Framework;
 
 using Gee.External.Capstone.M68K;
@@ -14,10 +16,7 @@ public class Field(Pos2D pos, FieldT type, char symbol, Dir? dir = null)
     public FieldT Type = type;
     public Dir? Dir = dir;
     public bool Loop;
-    public bool Considered = false;
-    public bool IsOutside;
     public bool Inner;
-    public bool Opening;
     public char Symbol = symbol;
     public GD? GD;
 }
@@ -29,8 +28,6 @@ public class Solution10 : SolutionFramework
     
      List<Field> loop = new();
 
-     record ExpectedPipeRecord(Pos2D P1, char P1S, Pos2D P2, char P2S);
-    
     public override string[] Solve()
     {
          var pipes = new Dictionary<char, Dir>()
@@ -43,8 +40,6 @@ public class Solution10 : SolutionFramework
              {'7', Dir.SW},
              {'F', Dir.SE},
          };
-
-
 
          var gr = (InpNl.Length, InpNl[0].Length).CreateGrid<Field>();
          var startPos = new Pos2D(0,0);
@@ -81,6 +76,7 @@ public class Solution10 : SolutionFramework
 
          var c = 0;
          var p = startPos;
+         gr.GetCell(startPos).Symbol = '|';
          var adj = GetConnections(startPos).Skip(1).First();
          GD gd;
          if (adj.pos.Y == startPos.Y)
@@ -92,77 +88,12 @@ public class Solution10 : SolutionFramework
              gd = adj.pos.Y > startPos.Y ? GD.S : GD.N;
          }
          p = adj.pos;
-         Console.WriteLine(gd.ToString());
-         
          
          while (true)
          {
              var cell = gr.GetCell(p);
-             var pos = cell.Pos;
              var dir = cell.Dir;
-             var s = cell.Symbol;
              
-             switch (s)
-             {
-                 case 'L':
-                 {
-                     switch (gd)
-                     {
-                         case GD.N:
-                             try { gr.GetCell(pos with { X = pos.X + 1, Y = pos.Y + 1 }).Inner = true; }
-                             catch { }
-                             break;
-
-                         case GD.W:
-                             try { gr.GetCell(pos with { X = pos.X - 1, Y = pos.Y - 1 }).Inner = true; }
-                             catch { }
-                             break;
-                     }
-                     break;
-                 }
-
-                 case 'J':
-                     switch (gd)
-                     {
-                         case GD.E:
-                             try { gr.GetCell(pos with { X = pos.X - 1, Y = pos.Y - 1 }).Inner = true; }
-                             catch { }
-                             break;
-                         case GD.N:
-                             try { gr.GetCell(pos with { X = pos.X + 1, Y = pos.Y + 1 }).Inner = true; }
-                             catch { }
-                             break;
-                     }
-                     break;
-                 case '7':
-                     switch (gd)
-                     {
-                         case GD.E:
-                             try { gr.GetCell(pos with { X = pos.X + 1, Y = pos.Y + 1 }).Inner = true; }
-                             catch { }
-                             break;
-                         case GD.S:
-                             try { gr.GetCell(pos with { X = pos.X - 1, Y = pos.Y - 1 }).Inner = true; }
-                             catch { }
-                             break;
-                     }
-                     break;
-                 case 'F':
-                     switch (gd)
-                     {
-                         case GD.N:
-                             try { gr.GetCell(pos with { X = pos.X - 1, Y = pos.Y - 1 }).Inner = true; }
-                             catch { }
-                             break;
-                         case GD.W:
-                             try { gr.GetCell(pos with { X = pos.X + 1, Y = pos.Y + 1 }).Inner = true; }
-                             catch { }
-                             break;
-                     }
-                     break;
-             }
-
-
              switch (gd)
              {
                  case GD.E when dir is Dir.SW or Dir.NW:
@@ -179,7 +110,6 @@ public class Solution10 : SolutionFramework
                      break;
              }
 
-             
              p = (gd) switch
              {
                  GD.N => p with { Y = p.Y - 1},
@@ -199,33 +129,54 @@ public class Solution10 : SolutionFramework
          }
          AssignAnswer1(c/2);
 
-         var nonInner = 0;
+         var outer = 0;
          var inner = 0;
          
-         //DetermineOpenings();
-
          for (var i = 0; i < gr.Length; i++)
          {
+             var openings = new Stack<char>();
+             var nEdges = 0;
+                 
              for (var j = 0; j < gr[i].Length; j++)
              {
                  var pos = new Pos2D(j, i);
                  var cell = gr.GetCell(pos);
-
-                 if (cell is { Loop: false, Considered: false })
+                 
+                 if (!cell.Loop)
                  {
-                     var area = GetSeparatedGroundArea(pos);
-                     var isInner = !area.Items.Any(x => x.Inner);
-                     if (isInner)
+                     if (nEdges % 2 == 0)
                      {
-                         inner += area.Items.Length;
+                         outer++;
+                         Console.Write('.');
                      }
-                     
-                     foreach (var kv in area.Items)
+                     else
                      {
-                         kv.Inner = !kv.Loop && isInner;
+                         Console.Write('I');
+                         inner++;
                      }
                  }
-                 Console.Write(cell.Inner ? "I" : cell.Loop ? cell.Symbol : 'O');
+                 else
+                 {
+                     Console.Write(cell.Symbol);
+                     if (cell.Symbol is 'L' or 'F' or 'S')
+                     {
+                         openings.Push(cell.Symbol);
+                     }
+                     else
+                     {
+                         openings.TryPeek(out var lastOpening);
+                         if (lastOpening is 'L' && cell.Symbol is '7' or 'S' ||
+                             lastOpening is 'F' && cell.Symbol is 'J' or 'S')
+                         {
+                             nEdges++;
+                             openings.Pop();
+                         }
+                         if (cell.Symbol is '|')
+                         {
+                             nEdges++;
+                         }
+                     }
+                 }
              }
              Console.WriteLine();
          }
@@ -233,126 +184,14 @@ public class Solution10 : SolutionFramework
         
          Console.WriteLine("Dim " + (InpNl.Length, InpNl[0].Length));
          Console.WriteLine("PL " + loop.Count);
-         Console.WriteLine((InpNl.Length*InpNl[0].Length-loop.Count-inner-nonInner));
-         Console.WriteLine(nonInner);
+         Console.WriteLine((InpNl.Length*InpNl[0].Length-loop.Count-inner-outer));
+         Console.WriteLine(outer);
          Console.WriteLine(inner);
          Console.WriteLine();
          AssignAnswer2(inner);
          
          return Answers;
          
-         
          IEnumerable<(Pos2D pos, Field val)> GetConnections(Pos2D p) =>gr.GetAllAdjacentCells(p, false).Where(c => c.val.Type is FieldT.Pipe);
-
-         void DetermineOpenings()
-         {
-                     //VERT
-             for (var i = 0; i < gr.Length-1; i++)
-             {
-                 for (var j = 0; j < gr[i].Length; j++)
-                 {
-                     var pos = new Pos2D(j, i);
-                     var c1 = gr.GetCell(pos);
-                     var c2 = gr.GetCell(pos with{Y = pos.Y+1});
-                     if (!(c1.Loop && c2.Loop))
-                     {
-                         continue;
-                     }
-                     var joint = c1.Symbol.ToString() + c2.Symbol;
-                     if (joint is "--" or "JF" or "L7" or "-F" or "-7" or "L-" or "J-" or "||" or "|L" or "|J" or "FL" or "7|" or "7L" or "7J")
-                     {
-                         c1.Opening = true;
-                         c2.Opening = true;
-                     }
-                 }
-             }
-                     //HOR
-             for (var i = 0; i < gr.Length; i++)
-             {
-                 for (var j = 0; j < gr[i].Length-1; j++)
-                 {
-                     var pos = new Pos2D(j, i);
-                     var c1 = gr.GetCell(pos);
-                     var c2 = gr.GetCell(pos with{X = pos.X+1});
-                     if (!(c1.Loop && c2.Loop))
-                     {
-                         continue;
-                     }
-                     var joint = c1.Symbol.ToString() + c2.Symbol;
-                     if (joint is "||" or "7F" or "JL" or "J|" or "|F" or "7|" or "J|" or "|L" or "S-" or "-7" or "F-" or "LJ" or "7L" or "-7" or "--" or "F7" or "S7")
-                         
-                     {
-                         c1.Opening = true;
-                         c2.Opening = true;
-                     }
-                 }
-             }
-                     
-         }
-
-         (Field[] Items, bool hasOpening) GetSeparatedGroundArea(Pos2D pos)
-         {
-             var cell = gr.GetCell(pos);
-             var area = new List<Field>() {};
-             var st = new Stack<Field>();
-             
-             st.Push(cell);
-             var open = false;
-
-             while (st.Any())
-             {
-                 cell = st.Pop();
-                 area.Add(cell);
-                 cell.Considered = true;
-
-                 var adj1 = gr.GetAllAdjacentCells(cell.Pos, false).Where(kv=>kv.val is { Loop: false, Considered: false }); 
-                 foreach (var kv in adj1)
-                 {
-                     if (!area.Contains(kv.val))
-                     {
-                         st.Push(kv.val);
-                     }
-                 }
-             }
-             
-             return (area.ToArray(), area.Any(x => x.Pos.X == 0 || x.Pos.Y == 0 || x.Pos.X == gr[0].Length || x.Pos.Y == gr.Length));
-         }
-         
-         bool CanPass(GD gd, Pos2D p1, Pos2D p2)
-         {
-             Field? c1, c2;
-             try
-             {
-                 c1 = gr.GetCell(p1);
-                 c2 = gr.GetCell(p2);
-             }
-             catch
-             {
-                 return false;
-             }
-             var pipe = c1.Symbol.ToString() + c2.Symbol;
-
-             switch (gd)
-             {
-                 case GD.N:
-                     return pipe is "||" or "7F" or "|F" or "7|";
-                 case GD.S:
-                     return pipe is "||" or "JL" or "|L" or "J|";
-                 case GD.W:
-                     return pipe is "--" or "L7" or "L-" or "-7";
-                 case GD.E:
-                     return pipe is "--" or "JF" or "J-" or "-F";
-                 case GD.BL:
-                     return pipe is "JF";
-                 case GD.BR:
-                     return pipe is "7L";
-                 case GD.TL:
-                     return pipe is "L7";
-                 case GD.TR:
-                     return pipe is "JF";
-             }
-             return false;
-         }
-         
     }
 }
--- Solutions2023/13/Solution13.cs
+++ Solutions2023/13/Solution13.cs
@@ -8,6 +8,225 @@ public class Solution13 : SolutionFramework
     
     public override string[] Solve()
     {
+        var grids = new List<char[][]>();
+
+        var temp = new List<string>();
+        foreach (var s in InpNl)
+        {
+            if (s != string.Empty)
+            {
+                temp.Add(s);
+            }
+            else
+            {
+                ParseSection(grids, temp);
+                temp = new List<string>();
+            }
+        }
+        ParseSection(grids, temp);
+        
+        List<(string A, int[] B)> conditionRecords = new string[]{}.Select(line => //File.ReadAllLines(@"..\..\..\..\day-12.txt").Select(line =>
+        {
+            var parts = line.Split(" ");
+            //return new { springs = parts[0], groups = parts[1].Split(',').Select(int.Parse).ToArray() };
+            return (parts[0], parts[1].Split(',').Select(int.Parse).ToArray());
+        }).ToList(); 
+        
+        var r = new List<int>();
+        var c = new List<int>();
+        foreach (var gr in grids)
+        {
+            for (var i = 0; i < gr.Length-1; i++)
+            {
+                if (IsSame(gr,i, i+1))
+                {
+                    var offset = 0;
+                    var isSame = true;
+                    while (i+1+offset < gr.Length-1 && i - ++offset >= 0 && (isSame = IsSame(gr, i-offset, i + 1 + offset))){ }
+                    if (isSame)
+                    {
+                        r.Add(i+1);
+                    }
+                }
+            }
+            for (var j = 0; j < gr[0].Length-1; j++)
+            {
+                var (vl1, vl2) = GetVerticalLines(gr, j, j + 1);
+                if(IsSame(vl1, vl2))
+                {
+                    var offset = 0;
+                    var isSame = true;
+                    while (j + 1 + offset < gr[0].Length - 1 && j - ++offset >= 0)
+                    {
+                        (vl1, vl2) = GetVerticalLines(gr, j-offset, j + 1 + offset);
+                        isSame = IsSame(vl1, vl2);
+                        if (!isSame)
+                        {
+                            break;
+                        }
+                    }
+                    if (isSame)
+                    {
+                        c.Add(j+1);
+                    }
+                }
+            }
+        }
+        Console.WriteLine();
+        AssignAnswer1(r.Sum()*100 + c.Sum());
+        
+        r = new List<int>();
+        c = new List<int>();
+        foreach (var gr in grids)
+        {
+            var found = false;
+            for (var i = 0; i < gr.Length-1; i++)
+            {
+                var diffUsed = Has1Diff(gr, i, i + 1);
+                if (IsSame(gr,i, i+1) || diffUsed)
+                {
+                    var offset = 0;
+                    var isSame = true;
+                    while (i+1+offset < gr.Length-1 && i - ++offset >= 0)
+                    {
+                        isSame = IsSame(gr, i - offset, i + 1 + offset);
+                        if (!isSame)
+                        {
+                            if (!diffUsed && Has1Diff(gr, i - offset, i + 1 + offset))
+                            {
+                                isSame = true;
+                                diffUsed = true;
+                            }
+                        }
+                        if (!isSame)
+                        {
+                            break;
+                        }
+                    }
+                    if (isSame && diffUsed)
+                    {
+                        r.Add(i+1);
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+            if (found)
+            {
+                continue;
+            }
+            found = false;
+            for (var j = 0; j < gr[0].Length-1; j++)
+            {
+                var (vl1, vl2) = GetVerticalLines(gr, j, j + 1);
+                var diffUsed = Has1Diff(vl1,vl2);
+                if(IsSame(vl1, vl2) || diffUsed)
+                {
+                    var offset = 0;
+                    var isSame = true;
+                    while (j + 1 + offset < gr[0].Length - 1 && j - ++offset >= 0)
+                    {
+                        (vl1, vl2) = GetVerticalLines(gr, j-offset, j + 1 + offset);
+                        isSame = IsSame(vl1 ,vl2);
+                        if (!isSame)
+                        {
+                            if (!diffUsed && Has1Diff(vl1,vl2))
+                            {
+                                isSame = true;
+                                diffUsed = true;
+                            }
+                        }                    
+                        if (!isSame)
+                        {
+                            break;
+                        }
+                    }
+                    if (isSame && diffUsed)
+                    {
+                        c.Add(j+1);
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+        }
+        
+        
+        AssignAnswer2(r.Sum()*100 + c.Sum());
         return Answers;
+    }
+    
+    private static (char[] vl1, char[] vl2) GetVerticalLines(char[][] gr, int c1, int c2)
+    {
+        var vl1 = new char[gr.Length];
+        var vl2 = new char[gr.Length];
+        for (var i = 0; i < gr.Length; i++)
+        {
+            vl1[i] = gr[i][c1];
+            vl2[i] = gr[i][c2];
+        }
+        return (vl1, vl2);
+    }
+
+    private static bool IsSame(char[][] gr, int r1, int r2)
+    {
+        var s1 = new string(gr[r1]);
+        var s2 = new string(gr[r2]);
+        return s1 == s2;
+    }
+    private static bool IsSame(char[] line1, char[] line2)
+    {
+        var s1 = new string(line1);
+        var s2 = new string(line2);
+        return s1 == s2;
+    }
+    
+    private static bool Has1Diff(char[][] gr, int r1, int r2)
+    {
+        var s1 = new string(gr[r1]);
+        var s2 = new string(gr[r2]);
+        return has1Diff(s1, s2);
+    }
+    private static bool Has1Diff(char[] line1, char[] line2)
+    {
+        var s1 = new string(line1);
+        var s2 = new string(line2);
+        return has1Diff(s1, s2);
+    }
+    
+    private static bool has1Diff(string s1, string s2)
+    {
+        var diff = 0;
+        for (var i = 0; i < s1.Length; i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                diff++;
+            }
+            if (diff > 1)
+            {
+                return false;
+
+            }
+        }
+        return diff == 1;
+    }
+
+    private static void ParseSection(List<char[][]> grids, List<string> temp)
+    {
+        grids.Add((temp.Count, temp[0].Length).CreateGrid<char>());
+        for (var i = 0; i < temp.Count(); i++)
+        {
+            for (var j = 0; j < temp[0].Length; j++)
+            {
+                grids.Last()[i][j] = temp[i][j];
+            }
+        }
     }
 }
